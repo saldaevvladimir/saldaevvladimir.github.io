9ee1ed5c9134935f14cf85c4296ea5c6d3341a8a
saldaev vladimir <caldaevvlad3@gmail.com>
2024-04-08 20:19:14 +1000

    added invert mode

--- static/script.js
+++ static/script.js
@@ -1,4 +1,16 @@
 
+const container = document.getElementsByClassName('container')[0];
+const switcher = document.querySelector('input');
+switcher.checked = false;
+
+function ChangeInvertMode() {
+    if (!switcher.checked) {
+        container.classList.remove('invert');
+    } else {
+        container.classList.add('invert');
+    }
+}
+
 (function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
         window.setTimeout(callback, 1000 / 60);
@@ -17,66 +29,7 @@ var background = document.getElementById("bgCanvas"),
 background.width = width;
 background.height = height;
 
-function Terrain(options) {
-    options = options || {};
-    this.terrain = document.createElement("canvas");
-    this.terCtx = this.terrain.getContext("2d");
-    this.scrollDelay = options.scrollDelay || 90;
-    this.lastScroll = new Date().getTime();
-
-    this.terrain.width = width;
-    this.terrain.height = height;
-    this.fillStyle = options.fillStyle || "#191D4C";
-    this.mHeight = options.mHeight || height;
-
-    // generate
-    this.points = [];
-
-    var displacement = options.displacement || 140,
-        power = Math.pow(2, Math.ceil(Math.log(width) / (Math.log(2))));
-
-    // set the start height and end height for the terrain
-    this.points[0] = this.mHeight;//(this.mHeight - (Math.random() * this.mHeight / 2)) - displacement;
-    this.points[power] = this.points[0];
-
-    // create the rest of the points
-    for (var i = 1; i < power; i *= 2) {
-        for (var j = (power / i) / 2; j < power; j += power / i) {
-            this.points[j] = ((this.points[j - (power / i) / 2] + this.points[j + (power / i) / 2]) / 2) + Math.floor(Math.random() * -displacement + displacement);
-        }
-        displacement *= 0.6;
-    }
-
-    document.body.appendChild(this.terrain);
-}
-
-Terrain.prototype.update = function () {
-    // draw the terrain
-    this.terCtx.clearRect(0, 0, width, height);
-    this.terCtx.fillStyle = this.fillStyle;
-
-    if (new Date().getTime() > this.lastScroll + this.scrollDelay) {
-        this.lastScroll = new Date().getTime();
-        this.points.push(this.points.shift());
-    }
-
-    this.terCtx.beginPath();
-    for (var i = 0; i <= width; i++) {
-        if (i === 0) {
-            this.terCtx.moveTo(0, this.points[0]);
-        } else if (this.points[i] !== undefined) {
-            this.terCtx.lineTo(i, this.points[i]);
-        }
-    }
-
-    this.terCtx.lineTo(width, this.terrain.height);
-    this.terCtx.lineTo(0, this.terrain.height);
-    this.terCtx.lineTo(0, this.points[0]);
-    this.terCtx.fill();
-}
-
-
-// Second canvas used for the stars
+// canvas used for the stars
 bgCtx.fillStyle = '#05004c';//'#05004c'
 bgCtx.fillRect(0, 0, width, height);
 
@@ -172,9 +125,6 @@ for (var i = 0; i < height; i++) {
 for (var i = 0; i < 8; i++) {
     entities.push(new ShootingStar());
 }
-entities.push(new Terrain({ mHeight: (height / 2) - 120 }));
-entities.push(new Terrain({ displacement: 120, scrollDelay: 50, fillStyle: "rgb(17,20,40)", mHeight: (height / 2) - 60 }));
-entities.push(new Terrain({ displacement: 100, scrollDelay: 20, fillStyle: "rgb(10,10,5)", mHeight: height / 2 }));
 
 function fadeIn(element, duration) {
     return new Promise(function (resolve) {
